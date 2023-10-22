1b32f335c74980e5071cc312099436d28de34a12
mcastro-101 <148722555+mcastro-101@users.noreply.github.com>
2023-10-22 14:08:19 -0700

    Add files via upload

---
+++ mdc12-assignment0/src/model/Castro.java
@@ -0,0 +1,19 @@
+/*
+ * TCSS 305 Autumn 2022
+ * Assignment 0
+ */
+
+package model;
+
+/**
+ * This program displays some output about myself.
+ * @author Michael Castro
+ * @version 7 October 2022
+ */
+public class Castro {
+    public void printCastroInfo() {
+        System.out.println("I am 23 years old.");
+        System.out.println("I am current a UWT Student.");
+        System.out.println("My favoite superhero is Spider-Man.");
+    }
+}
